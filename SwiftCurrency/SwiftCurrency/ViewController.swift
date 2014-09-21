ed7718a7b11d68bfb81b068f85ecda4364ef03be
man27382210 <man27382210@gmail.com>
2014-09-21 22:07:18 +0800

    add info view, add UIRefresh controller

--- SwiftCurrency/SwiftCurrency/ViewController.swift
+++ SwiftCurrency/SwiftCurrency/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableCurrecny:UITableView!
+    var refreshController:UIRefreshControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshController = UIRefreshControl()
+        tableCurrecny.addSubview(refreshController)
+        refreshController.addTarget(self, action: "tableRefresh", forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell!
+        if(indexPath.section == 0){
+            cell = tableCurrecny.dequeueReusableCellWithIdentifier("CellMainCurrency") as UITableViewCell
+        }else{
+            cell = tableCurrecny.dequeueReusableCellWithIdentifier("CellShowCurrency") as UITableViewCell
+        }
+        
+        return cell
+    }
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if(section == 0){
+            return 20;
+        }else{
+            return 20;
+        }
+    }
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if(section == 0){
+            return "Main"
+        }
+        return "Other   "
+    }
+    func tableRefresh(){
+        refreshController.endRefreshing()
+        tableCurrecny.reloadData()
+    }
+    
+    @IBAction func closeFromInfo(sender:UIStoryboardSegue){
+        
+    }
 }
 
