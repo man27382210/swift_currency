1f2658324282ad16694b03d9515ca117229a93a8
man27382210 <man27382210@gmail.com>
2014-09-28 20:42:35 +0800

    change to new design

--- SwiftCurrency/SwiftCurrency/ViewController.swift
+++ SwiftCurrency/SwiftCurrency/ViewController.swift
@@ -28,29 +28,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        } else {
             return SCModel.getInstance.comparings.count
-        }
+
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cellIdentifier: NSString = (indexPath.section == 0) ? "CellMainCurrency" : "CellShowCurrency"
+        var cellIdentifier: NSString = "CellShowCurrency"
         let cell: SCCurrencyCell! = currencyTable.dequeueReusableCellWithIdentifier(cellIdentifier) as SCCurrencyCell
-        let ccy = (indexPath.section == 0) ? SCModel.getInstance.selected! : SCModel.getInstance.comparings[indexPath.row]
+        let ccy = SCModel.getInstance.comparings[indexPath.row]
         cell.flagImageView.image = UIImage(named: ccy.imageName)
         cell.codeTextLabel.text = ccy.code()
         cell.fullNameTextLabel.text = ccy.fullName
-        if let mainCell: SCMainCurrencyCell = cell as? SCMainCurrencyCell {
-            mainCell.rateInput.text = NSString(format: "%f", ccy.rate)
-        } else {
-            cell.rateLabel!.text = NSString(format: "%f", ccy.rate)
-        }
+        cell.rateLabel!.text = NSString(format: "%f", ccy.rate)
         return cell
     }
 
@@ -59,7 +52,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let key: NSString = (section == 0) ? "table_section_title_main" : "table_section_title_other"
+        let key: NSString = "table_section_title_other"
         return NSLocalizedString(key, comment: "")
     }
     
